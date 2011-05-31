c922ef4a7b3d812faf5baf12807342287a88b43d
Patrick Lightbody <patrick@lightbody.net>
2011-05-30 19:08:49 -0700

    getting ready for an official release! using Maven guidelines documented here: https://docs.sonatype.org/display/Repository/Sonatype+OSS+Maven+Repository+Usage+Guide

--- etc/install.sh
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -DgroupId=com.google.code.guice -DartifactId=guice-servlet -Dversion=1.0 -Dpackaging=jar -Dfile=guice-servlet-1.0.jar
-mvn install:install-file -DgroupId=org.directwebremoting -DartifactId=dwr -Dversion=3.0.0.109.dev -Dpackaging=jar -Dfile=dwr-3.0.0.109.dev.jar
