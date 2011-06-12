78f0d2c71fa26f32529b49c5ba227a1162e061c5
Mark Watson <watsonmw@gmail.com>
2011-06-13 05:28:49 +0800

    Prevent blacklisted content from bombing out the proxy.
    Also report "NO RESPONSE", instead of "INTERNAL STATE" for the status
    code text when a request was not made by the proxy.

--- src/main/java/org/browsermob/proxy/http/BrowserMobHttpClient.java
+++ src/main/java/org/browsermob/proxy/http/BrowserMobHttpClient.java
@@ -457,7 +457,7 @@ public class BrowserMobHttpClient {
         // we still have the attempt associated, even if we never got a response
         HarEntry entry = new HarEntry(harPageRef);
         entry.setRequest(new HarRequest(method.getMethod(), url, method.getProtocolVersion().getProtocol()));
-        entry.setResponse(new HarResponse(-999, "INTERNAL STATE", method.getProtocolVersion().getProtocol()));
+        entry.setResponse(new HarResponse(-999, "NO RESPONSE", method.getProtocolVersion().getProtocol()));
         if (this.har != null && harPageRef != null) {
             har.getLog().addEntry(entry);
         }
@@ -588,7 +588,9 @@ public class BrowserMobHttpClient {
         entry.getResponse().setBodySize(bytes);
         entry.getResponse().getContent().setSize(bytes);
         entry.getResponse().setStatus(statusCode);
-        entry.getResponse().setStatusText(statusLine.getReasonPhrase());
+        if (statusLine != null) {
+            entry.getResponse().setStatusText(statusLine.getReasonPhrase());
+        }
 
         boolean urlEncoded = false;
         if (captureHeaders || captureContent) {
